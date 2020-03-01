e9c8043e31eea5a1ec3d0fcdf19c9c2be1985ef6
tom.parker <tom.parker@everest-media.co.uk>
2020-03-01 14:18:34 +0000

    no message

--- components/SiteNav.vue
+++ components/SiteNav.vue
@@ -6,8 +6,8 @@
   >
     <div class="navbar-brand">
       <nuxt-link class="navbar-item" to="/">
-        <site-logo v-if="$siteConfig.logo === 'logo-component'" />
-        <!--<img
+        <!--<site-logo v-if="$siteConfig.logo === 'logo-component'" />
+        <img
           v-else
           :src="$siteConfig.logo"
           :alt="$siteConfig.siteName"
